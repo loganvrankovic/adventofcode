545b4a2860284f5ddab209bbbaea9a63a87016ce
Logan Vrankovic <loganvrz@gmail.com>
2023-12-17 17:13:58 -0500

    updated day 8 part 1

--- 2023/day8/part1.js
+++ 2023/day8/part1.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const data = fs.readFileSync('example.txt', 'utf-8').split(/\r?\n/);
+const data = fs.readFileSync('example2.txt', 'utf-8').split(/\r?\n/);
 
 const inst = data.slice(0, 1).join('').split('');
 const mapArr = data.slice(2)
@@ -13,25 +13,21 @@ let currEl = 'AAA';
 
 while (currEl !== 'ZZZ') {
     for (let i = 0; i < inst.length + 1; i++) {
-        let index = 0; // find index of the current element
+        let index = 0;
         for (let j = 0; j < mapArr.length; j++) {
             if (mapArr[j][0] === currEl) {
                 index = j;
             }
         }
-        // change the current element according to the instructions
-        for (let j = index; j < mapArr.length; j++) {
-            if (inst[i] === 'L') {
-                currEl = mapArr[j][1][0]
-                steps++;
-                break;
-            } else if (inst[i] === 'R') {
-                currEl = mapArr[j][1][1]
-                steps++;
-                break;        
-            }
+
+        if (inst[i] === 'L') {
+            currEl = mapArr[index][1][0]
+            steps++;
+        } else if (inst[i] === 'R') {
+            currEl = mapArr[index][1][1]
+            steps++;     
         }
     }
 }
 
-console.log(steps)
+console.log("Steps from AAA to ZZZ: " + steps)
